61b5bb830078f9b69f98faa73a3dff4d65093308
vinozganic <jakov.vinozganic@fer.hr>
2024-11-16 11:08:28 +0100

    Update game visuals and functionality: change canvas background color, streamline HTML structure, and enhance scoring system

--- ferBreakout.js
+++ ferBreakout.js
@@ -1,49 +1,5 @@
-//** Igra se prikazuje u Canvas objektu koji pokriva cijeli prozor web preglednika. Pozadina Canvas može biti crne ili bijele boje, ili neke druge boje po vašem
-//** odabiru s dovoljnim kontrastom za bolji prikaz.
-
-//** Canvas objekt mora imati vidljivi rub.
-
-//** Igra započinje odmah nakon učitavanja web stranice. Na početku igre generira se određen broj objekata (cigli) koje igrač treba razbiti pomoću loptice koja se
-//** odbija o palicu koju igrač kontrolira na dnu ekrana. Loptica se inicijalno generira na središtu palice i počinje se kretati prema gore pod slučajnim kutem.
-
-//** Igrač upravlja palicom obavezno pomoću tipkovnice. Npr. tipke strelice - lijevo i desno, ili neke druge dvije tipke po vašem izboru.
-
-//** Novi objekti cigli su raspoređeni u nekoliko redova na vrhu ekrana, dok se loptica i palica nalaze na dnu. Loptica se kreće konstantnom brzinom, a smjer mijenja
-//** prilikom udara o palicu, cigle ili rubove ekrana. Odabrane vrijednosti za brzinu loptice i veličinu palice moraju biti takve da je igru moguće igrati, odnosno
-//** da nije preteška.
-
-//** Parametri igre - broj cigli i početna brzina loptice su predefinirani (konstantne u vašem programskom kodu). Opcionalno, parametri igre mogu se konfigurirati kroz
-//** HTML5 web stranicu prije pokretanja igre.
-
-//** Objekt koji predstavlja ciglu je pravokutnik odabrane boje. Umjesto pravokutnika dozvoljeno je koristiti prikladnu sliku (JPG, PNG) cigle.
-
-//** Slično, objekt koji predstavlja palicu na dnu ekrana je pravokutnik crvene boje s obaveznim sjenčanjem ruba. Dozvoljeno je koristiti neku sliku (npr. platformu)
-//** umjesto pravokutnika.
-
-//** Cigle i palica obavezno moraju imati sjenčanje ruba i biti prikazani korištenjem HTML5 Canvas API-a.
-
-//** Ako loptica izađe izvan donjeg ruba ekrana, igra završava, te se obavezno preko sredine Canvasa (vertikalno i horizontalno centrirano) prikazuje tekstualna
-//** obavijest (npr. "GAME OVER"), velikih slova u definiranom fontu.
-
-//** Ako je igrač dovoljno vješt i uspije uništiti sve cigle, igra završava uz obavezni ispis prikladne poruke na isti način.
-
-//** U svakom koraku animacije mora se detektirati kolizija (sudar) loptice s ciglama, palicom i rubovima ekrana. Nakon svakog sudara s ciglom, cigla nestaje, a igrač
-//** dobiva bodove (1 uništena cigla = 1 bod).
-
-//** Moguće je (nije obavezno) generirati zvuk prilikom kolizije loptice i cigle, loptice i palice, ili loptice i gornjeg, lijevo i desnog ruba Canvasa. Također,
-//** moguće je generirati odgovarajuće zvuk na početku i kraju igre, te kada igrač pređe neki okrugli broj bodova.
-
-// Igra mora mjeriti broj bodova, a cilj igrača je postići što više bodova razbijanjem svih cigli bez gubitka loptice.
-
-// Najbolje ostvareni rezultat, od kad je igra prvi put pokrenuta, mora se pohranjivati koristeći local storage pomoću HTML5 Web Storage API-ja.
-
-// U gornjem desnom rubu Canvasa mora se prikazivati trenutni broj bodova i maksimalni broj bodova.
-
-// Deklaracije struktura, objekata, funkcija kao i pozivi funkcija moraju biti detaljno komentirani i precizno objašnjeni u HTML5 i JavaScript izvornom kodu vašim
-// riječima.
-
 /*======================
-Number constants
+   Brojevne konstante
 ========================*/
 const BALL_RADIUS = 10;
 const BALL_SPEED = 8;
@@ -51,8 +7,8 @@ const BALL_Y_OFFSET = 10;
 
 const STICK_WIDTH = 250;
 const STICK_HEIGHT = 10;
-const STICK_SPEED = 10;
-const STICK_MOVE_MULTIPLIER = 5;
+const STICK_SPEED = 13;
+const STICK_MOVE_MULTIPLIER = 2;
 const STICK_Y_OFFSET = 20;
 
 const BRICK_ROW_COUNT = 2;
@@ -63,8 +19,12 @@ const BRICK_PADDING = 10;
 const BRICK_OFFSET_TOP = 50;
 const BRICK_OFFSET_LEFT = 10;
 
+const SCORE_X_OFFSET = 80;
+const HIGHSCORE_X_OFFSET = 240;
+const SCORE_HIGHSCORE_Y_OFFSET = 20;
+
 /*======================
-Class definitions
+    Definicije klasa
 ========================*/
 class Ball {
     constructor(x, y, radius, directionX, directionY, speed) {
@@ -112,6 +72,9 @@ class Stick {
     }
 
     move(direction) {
+        const x = this.x + this.speed * STICK_MOVE_MULTIPLIER * direction;
+        if (x >= canvas.clientWidth - this.width || x <= 0) return;
+
         this.x += this.speed * STICK_MOVE_MULTIPLIER * direction;
     }
 }
@@ -148,17 +111,38 @@ class Brick {
             context.restore();
         }
     }
+
+    static drawBricks() {
+        bricks.forEach(brick => {
+            if (brick.status === "intact") {
+                brick.draw(context);
+                if (
+                    ball.x - ball.radius < brick.x + brick.width &&
+                    ball.x + ball.radius > brick.x &&
+                    ball.y - ball.radius < brick.y + brick.height &&
+                    ball.y + ball.radius > brick.y
+                ) {
+                    ball.directionY = -ball.directionY;
+                    brick.status = "destroyed";
+                    score++;
+                }
+            }
+        });
+    }
 }
 
 /*======================
-Global variables
+    Globalne varijable
 ========================*/
 let score = 0;
 let isGameRunning = false;
 let animationFrameId = null;
 
+let ball;
+const bricks = [];
+
 /*======================
-Main program setup
+  Inicializacija platna
 ========================*/
 const canvas = document.getElementById("breakout-canvas");
 const context = canvas.getContext("2d");
@@ -187,10 +171,8 @@ function createBall() {
     let randomDirection = getRandomDirection();
     return new Ball(initialPositionX, initialPositionY, BALL_RADIUS, randomDirection.x, randomDirection.y, BALL_SPEED);
 }
+ball = createBall();
 
-let ball = createBall();
-
-const bricks = [];
 function createBrickWall() {
     const brickWidth = canvas.width / BRICK_COLUMN_COUNT - BRICK_WIDTH_OFFSET;
     for (let c = 0; c < BRICK_COLUMN_COUNT; c++) {
@@ -204,29 +186,58 @@ function createBrickWall() {
 createBrickWall();
 
 /*======================
-Game control functions
+    Najbolji rezultat
+========================*/
+function getHighscore() {
+    const highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        localStorage.setItem("highscore", score);
+        return score;
+    }
+    return highScore;
+}
+
+function setHighscore() {
+    const highScore = getHighscore();
+    if (score > highScore) {
+        localStorage.setItem("highscore", score);
+    }
+}
+
+function renderScores() {
+    setHighscore();
+
+    context.font = "20px Arial";
+    context.fillStyle = "darkblue";
+    context.textAlign = "center";
+    context.fillText(`Current score: ${score}`, canvas.width - SCORE_X_OFFSET, SCORE_HIGHSCORE_Y_OFFSET);
+    context.fillText(`Highscore: ${getHighscore()}`, canvas.width - HIGHSCORE_X_OFFSET, SCORE_HIGHSCORE_Y_OFFSET);
+}
+
+/*======================
+      Kontrole igre
 ========================*/
 function resetGame() {
     stick.x = (canvas.width - stick.width) / 2;
     ball = createBall();
     bricks.forEach(brick => (brick.status = "intact"));
     score = 0;
-    document.getElementById("score").innerText = `Score: ${score}`;
+    renderScores();
 }
 
 function gameLost() {
     isGameRunning = false;
     cancelAnimationFrame(animationFrameId);
-    displayMessage("GAME OVER", "red");
+    displayCentralMessage("GAME OVER", "red");
 }
 
 function gameWon() {
     isGameRunning = false;
     cancelAnimationFrame(animationFrameId);
-    displayMessage("YOU WON", "green");
+    displayCentralMessage("YOU WON", "green");
 }
 
-function displayMessage(message, color) {
+function displayCentralMessage(message, color) {
     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     context.font = "150px Arial";
     context.fillStyle = color;
@@ -234,7 +245,7 @@ function displayMessage(message, color) {
     context.fillText(message, canvas.width / 2, canvas.height / 2);
     context.font = "30px Arial";
     context.fillStyle = "darkblue";
-    context.fillText("Press any key to restart", canvas.width / 2, canvas.height / 2 + 100);
+    context.fillText("Press any keyboard key to restart", canvas.width / 2, canvas.height / 2 + 100);
     document.addEventListener("keydown", restartListener, { once: true });
 }
 
@@ -245,7 +256,7 @@ function restartListener() {
 }
 
 /*======================
-Main game loop
+   Glavna petlja igre
 ========================*/
 function infiniteGameLoop() {
     if (!isGameRunning) return;
@@ -272,31 +283,14 @@ function infiniteGameLoop() {
     if (bricks.every(brick => brick.status === "destroyed")) gameWon();
 
     stick.draw(context);
-    drawBricks();
+    Brick.drawBricks();
+    renderScores();
+
     requestAnimationFrame(infiniteGameLoop);
 }
 
-function drawBricks() {
-    bricks.forEach(brick => {
-        if (brick.status === "intact") {
-            brick.draw(context);
-            if (
-                ball.x - ball.radius < brick.x + brick.width &&
-                ball.x + ball.radius > brick.x &&
-                ball.y - ball.radius < brick.y + brick.height &&
-                ball.y + ball.radius > brick.y
-            ) {
-                ball.directionY = -ball.directionY;
-                brick.status = "destroyed";
-                score++;
-                document.getElementById("score").innerText = `Score: ${score}`;
-            }
-        }
-    });
-}
-
 /*======================
-Key listeners
+Claude: UNESI ISPRAVAN TEKST
 ========================*/
 document.addEventListener("keydown", event => {
     if (event.key === "ArrowLeft") stick.move(-1);
@@ -304,7 +298,7 @@ document.addEventListener("keydown", event => {
 });
 
 /*======================
-Image patterns
+    Učitavanje slika
 ========================*/
 const brickPatternImage = new Image();
 const stickPatternImage = new Image();
